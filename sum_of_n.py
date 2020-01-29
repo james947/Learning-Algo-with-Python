73edba5aab11422d21ee377678712f1cdb9ccafb
james947 <jmuriuki947@gmail.com>
2020-01-29 15:38:03 +0300

    Add faster solution

--- sum_of_n.py
+++ sum_of_n.py
@@ -4,4 +4,14 @@ def sum1(n):
     for i in range(n+1):
         final_sum +=i
     print(final_sum)
-sum1(5)
+sum1(1000)
+
+# the second method solves with a formula hence it's faster
+def sum2(n):
+    # take an input of n and return the sum of the numbers from 0 to n
+    return print((n*(n+1))/2)
+sum2(1000)
+
+# memory
+# time
+
